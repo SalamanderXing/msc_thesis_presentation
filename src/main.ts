cc4c89ee7eef5905b0c35d9769c872a007c1a205
salamandercrossing <giulio.zani@gmail.com>
2023-10-15 16:31:04 +0000

    slides

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,19 @@ import "./style.css";
 import Reveal from "reveal.js";
 import Markdown from "reveal.js/plugin/markdown/markdown.esm.js";
 
+const el = document.getElementsByClassName("reveal")[0];
+const mdSlides = await (await fetch("/slides.md")).text();
+el.innerHTML = `
+  <div class='slides'>
+    <section data-markdown>
+      <textarea data-template>
+        ${mdSlides} 
+      </textarea>
+    </section>
+  </div>
+`;
 let deck = new Reveal({
   plugins: [Markdown],
+  slideNumber: "c/t",
 });
 deck.initialize();
